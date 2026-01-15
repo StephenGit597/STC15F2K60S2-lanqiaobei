ef0c93e4edeac3e620829fc2223d1776ce704701
StephenGit597 <25391607@qq.com>
2026-01-15 23:58:08 +0800

    ADD-DS1302

---
+++ basic-module-codes/DS1302/ds1302.c
@@ -0,0 +1,47 @@
+static unsigned char hour=0;
+static unsigned char minute=0;
+static unsigned char second=0;
+//时间小时hour 分钟minute 秒second 采用全局变量
+//常用地址
+//写
+//0x80-秒
+//0x82-分钟
+//0x84-小时
+//0x8E-写保护处理 注0x00关闭写保护 0x80开启写保护
+
+//读
+//0x81-秒
+//0x83-分钟
+//0x85-小时
+//存储格式
+//八位BCD码
+//0 0 0 0 0 0 0 0 
+//--十位---个位---
+
+void DS1302_SetTime(unsigned char hour,unsigned char minute,unsigned char second)
+{
+	unsigned char temp=0;
+	if((hour>24)|(minute>60)|(second>60))
+	{
+		return;
+	}
+	DS1302_WriteCmd(0x8E,0x00);//关闭写保护
+	temp=((hour/10)<<4)|((hour%10)&0x0F);
+	DS1302_WriteCmd(0x84,temp);
+	temp=((minute/10)<<4)|((minute%10)&0x0F);
+	DS1302_WriteCmd(0x82,temp);
+	temp=((second/10)<<4)|((second%10)&0x0F);
+	DS1302_WriteCmd(0x80,temp);
+	DS1302_WriteCmd(0x8E,0x80);//恢复写保护
+}
+
+void DS1302_GetTime()
+{
+	unsigned char value=0;
+	value=DS1302_ReadCmd(0x85);
+	hour=10*(value>>4)+(value&0x0F);
+	value=DS1302_ReadCmd(0x83);
+	minute=10*(value>>4)+(value&0x0F);
+	value=DS1302_ReadCmd(0x81);
+	second=10*(value>>4)+(value&0x0F);
+}
