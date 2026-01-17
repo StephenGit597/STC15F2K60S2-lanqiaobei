1e44a164da0058a4857c29c2bf4ddaa9d0c93053
StephenGit597 <25391607@qq.com>
2026-01-17 18:37:26 +0800

    ADD-Timer-code

--- basic-driver/STC15F2K60S2定时器配置指南/Timer.c
+++ basic-driver/STC15F2K60S2定时器配置指南/Timer.c
@@ -26,7 +26,7 @@ void US_Delay(unsigned int us)//为阻塞延时
 	volatile unsigned int i=0;
 	for(i=0;i<us;i++)
 	{
-		_nop_();
+		_nop_();//8个_nop_()
 		_nop_();
 		_nop_();
 		_nop_();
@@ -35,4 +35,30 @@ void US_Delay(unsigned int us)//为阻塞延时
 		_nop_();
 		_nop_();
 	}
+}
+#定时器1-定时器配置(此蓝桥杯开发板定时器1只能配置为12T模式，配置为1T模式延时会延长)
+void Timer1Init(void)		//1毫秒@11.0592MHz
+{
+	AUXR &= 0xBF;		//定时器时钟12T模式
+	TMOD &= 0x0F;		//设置定时器模式
+	TL1 = 0x66;		//设置定时初值
+	TH1 = 0xFC;		//设置定时初值
+	TF1 = 0;		//清除TF1标志
+	TR1 = 1;		//定时器1开始计时
+	ET1=1;
+}
+
+void Timer1(void) interrupt 3
+{
+	currenttim++;
+}
+#定时器0配置为计数器，注计数脉冲来自P34，同时如果是定时器1引脚配置为计数器，计数脉冲来自P35
+void Timer0_Init(void)
+{
+	AUXR=0X80;//1分频
+	TMOD=0x05;//0 0 0 0 1 0 0 1，模式计数器模式，01需要手动的16位重装载模式
+	TH0=0;//TH0初值为0
+	TL0=0;//TL0初值为0
+	TR0=1;//使能计数器0
+	ET0=1//(按需)，如果需要计数器溢出中断则保留
 }
